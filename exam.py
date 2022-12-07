5159cca48306bcc44e85d78c26c7d2660cede2a1
Duyile22 <ogunduyileoyebola@gmail.com>
2022-12-07 02:55:34 +0000

    last commit

--- exam.py
+++ exam.py
@@ -1,5 +1,12 @@
 iGroup 10 members are amazing
 updated version
 please view
+<<<<<<< HEAD
 You did great Oyebola
+<<<<<<< HEAD
 Thanks guys
+=======
+=======
+God is great
+>>>>>>> e3a60c4 (modified)
+>>>>>>> 050b950 ('modified 1')
